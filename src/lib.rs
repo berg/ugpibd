cf27d0aaa2f4f88a860b9a8033c685311ddfd4db
Bryan Berg <berg@users.noreply.github.com>
2026-04-17 13:33:48 -0700

    chore: initial project scaffold

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 gpibd contributors
+pub mod firmware;
+pub mod gpib;
+pub mod protocol;
+pub mod prologix;
+pub mod server;
+pub mod usb;
